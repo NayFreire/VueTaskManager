83d1551dfcff97e1e95b4357adc2419d7036901a
Nayara Ribeiro Freire <nayfreireof@gmail.com>
2021-06-30 15:14:58 -0300

    Criando uma proxy para usar api/tasks ao invés de localhots:5000/tasks

--- src/App.vue
+++ src/App.vue
@@ -54,29 +54,27 @@ import AddTask from './components/AddTask'
 			toggleAddTask(){
 				this.showAddTask = !this.showAddTask
 				console.log(this.showAddTask)
+			},
+			async fetchTasks(){
+				//Getting the data from the file db.json
+				const res = await fetch('api/tasks')
+
+				const data = await res.json()
+
+				return data
+			},
+			async fetchTask(id){
+				//Getting the data from the file db.json
+				const res = await fetch(`api/tasks/${id}`)
+
+				const data = await res.json()
+
+				return data
 			}
 		},		
-		created(){
-			this.tasks = [
-				{
-					id: "1",
-					text: "Doctors Appointment",
-					day: "March 5th at 2:30pm",
-					reminder: true
-				},
-				{
-					id: "2",
-					text: "Meeting with boss",
-					day: "March 6th at 1:30pm",
-					reminder: true
-				},
-				{
-					id: "3",
-					text: "Food shopping",
-					day: "March 7th at 2:00pm",
-					reminder: false
-				}
-			]
+		async created(){ 
+			//Attribuing to tasks the data from the json file
+			this.tasks = await this.fetchTasks()
 		}
 	}
 </script>
