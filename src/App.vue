3491fbd302ada3848311b1172e98cbb4b699aa4f
Nayara Ribeiro Freire <nayfreireof@gmail.com>
2021-07-01 13:03:36 -0300

    Deletando tasks fdo arquivo db.json

--- src/App.vue
+++ src/App.vue
@@ -35,9 +35,15 @@ import AddTask from './components/AddTask'
 			}
 		},
 		methods:{
-			deleteTask(id){
+			async deleteTask(id){
 				if(confirm("Are you sure?")){
-					this.tasks = this.tasks.filter((task) => task.id !== id)
+					const res = await fetch(`api/tasks/${id}`, {
+						method: 'DELETE',
+					})
+
+					res.status === 200 ? (this.tasks = this.tasks.filter((task) => task.id !== id)) : alert('Error deleting task')
+
+					
 				}
 			},
 			toggleReminder(id){
